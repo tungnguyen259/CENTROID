1a8b05bc1461e92880ea33c435819e104910ff87
TungN <tung.nguyen@rainmaker-labs.com>
2017-06-16 15:32:41 +0700

    complete TC01 02

--- src/testcases/LoginScreen/Login.java
+++ src/testcases/LoginScreen/Login.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Listeners;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import commons.AbstractTest;
+import commons.CommonActions;
 import commons.Constant;
 import pages.HomePage;
 import pages.LoginPage;
@@ -15,8 +16,11 @@ public class Login extends AbstractTest{
 
 @Parameters({"browser"})
 @BeforeClass(alwaysRun = true)  
-  public void f (String browser) {
+  public void setup (String browser) {
 	  driver = openBrowser(browser);  
+	  dashboardPage = CommonActions.getCommonObject().getDataset("TC_Login", "pageName");
+	  loginSuccessfulMessage = CommonActions.getCommonObject().getDataset("TC_Login", "loginSuccessfulMessage");
+	  
   }
   
   @Test (description = "Verify user is albe to login with valid information")
@@ -33,15 +37,33 @@ public void TC_Login001 () {
 	loginPageObject.login(Constant.email, Constant.password, "No");
 	
 	log.info("VP: User is able to login successfully");
-	verifyTrue(homePageObject.checkPageDisplay(driver, pages));
+	verifyTrue(homePageObject.checkPageDisplay(driver, dashboardPage));
 	log.info("VP: Successful Message displays");
-	verifyTrue(homePageObject.checkSuccessfulMessage(message));
+	verifyTrue(homePageObject.checkSuccessfulMessage(loginSuccessfulMessage));
+	
+}
+  
+  @Test (description = "Verify user is unalbe to login with invalid information")
+public void TC_Login002 () {
+	    
+	log.info("Step 1: Open Centroid Webside");
+	navigateBrowser(driver, Constant.url);
+	
+	log.info("Step 2: Enter valid email and password");
+	log.info("Step 3: Click on 'Sign In' button");
+	loginPageObject.login(Constant.email, Constant.password, "No");
+	
+	log.info("VP: User is able to login successfully");
+	verifyTrue(homePageObject.checkPageDisplay(driver, dashboardPage));
+	
+	log.info("VP: Successful Message displays");
+	verifyTrue(homePageObject.checkSuccessfulMessage(loginSuccessfulMessage));
 	
 }
   
   private WebDriver driver;
   private LoginPage loginPageObject;
   private HomePage homePageObject;
-  private String pages = "Dashboard";
-  private String message ="Successfully logged in";
+  private String dashboardPage;
+  private String loginSuccessfulMessage;
 }
