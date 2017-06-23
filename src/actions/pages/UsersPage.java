e92fa2361c592576a32d6c1026c0d007ee15976b
TungN <tung.nguyen@rainmaker-labs.com>
2017-06-23 18:06:38 +0700

    a

--- src/actions/pages/UsersPage.java
+++ src/actions/pages/UsersPage.java
@@ -1,10 +1,7 @@
 package pages;
 
 import java.util.List;
-import java.util.NoSuchElementException;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
